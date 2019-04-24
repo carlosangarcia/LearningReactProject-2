5db7f0ff6605ea372e9b7170ea407749d5c390be
carlos <carloja@gmail.com>
2019-04-24 08:26:24 +0200

    inicio uso JSX

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import './App.css';
 import StatelessComponent from './StatelessComponent';
 import StatefulComponent from './StatefulComponent'
 class App extends Component {
-  render() {
-    return (  
-        <div>      
-            <StatelessComponent />
-            <StatefulComponent />
-        </div> 
-    );
-  }
-}
+
+    evento(e){
+        alert(e.target.value)
+    }
+    render() {
+        let calculo = 1 + 1009;
+
+        let style = null;
+        if(calculo == 10){
+            style = {color:'red'}
+        } else {
+            style = {color:'green'}
+        }
+
+        return (  
+            <div>
+              <h1>hola</h1>          
+              <input 
+              onClick={this.evento.bind(this)} 
+              type="txt" 
+              value="hola mundo" 
+              className="green"
+              style={style}/>
+              {calculo == 10 ? "10":"no 10"}
+            </div>
+        );
+      }
+    }
 
 export default App;
