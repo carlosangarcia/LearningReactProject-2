01edcbb7f36d9baf093400b72e5627a16ff66ef4
carlos <carloja@gmail.com>
2019-04-26 07:07:14 +0200

    comienzo practica propiedades

--- src/App.js
+++ src/App.js
@@ -1,40 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import StatelessComponent from './StatelessComponent';
-import StatefulComponent from './StatefulComponent'
+import Item from './Item';
+
 class App extends Component {
 
     constructor(args){
         super(args)
 
         this.state = {
-            counter: 32
+            users: []
         }
     }
 
-    sumar() {
+    add(e){
+        const nameInput = document.getElementById('name');
+        const roleInput = document.getElementById('role');
+        const telInput = document.getElementById('tel');
+
+        let newUser = {
+            name: nameInput.value,
+            role: roleInput.value,
+            tel: telInput.value
+        }
+        console.log(newUser);
+        
+        let users = this.state.users;
+
+        console.log(users);
+
+        users.unshift(newUser);
+
+        console.log(users);     
+
+        this.setState({users:users});
+
+        nameInput.value = '';
+        roleInput.value = '';
+        telInput.value = '';
+
+    }  
+
+    delete(id){
+        console.log("delete =>");
+
+        let userIndex = this.state.users.map(x => {return x.id}).indexOf(id)
+
+        let users = this.state.users
+
+        users.splice(userIndex, 1)
+
         this.setState({
-            counter: this.state.counter+1
+          users: users
         })
     }
-    
-    restar() {
-        this.setState({
-            counter: this.state.counter-1
-        })
-    }    
+
 
     render() {
 
         return (  
             <div>
-                <span>Contador {this.state.counter} </span>
-                <div>
-                    <button onClick={this.sumar.bind(this)}> + </button>
-                    <button onClick={this.restar.bind(this)}> - </button>
-                </div>
-            </div>
+                <label>Nombre</label>
+                <input id="name" type="text" />
+
+                <label>Rol</label>
+                <input id="role" type="text" />
+
+                <label>Tel</label>
+                <input id="tel" type="text" />
+
+                <button onClick={this.add.bind(this)}>Agregar</button>
+
+                <ul>
+                    {this.state.users.map(user => {
+                        return <Item key={user.id}
+                            id={user.id}
+                            user={user}
+                            deleteOp={this.delete.bind(this)}
+                            />
+
+                    })}
+                </ul>
+
+           </div>
         );
       }
     }
